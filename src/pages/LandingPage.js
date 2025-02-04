8ad296fe3404b2341cc4fd80ec1fc21adc5be98b
Farrah Isabelle <farrahisabelle@Farrahs-MacBook-Pro.local>
2025-02-04 10:23:22 -0500

    landing page start of css

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom"; // Ensure this is included
+import "../styles/LandingPage.css"
+import buttonImage from "../images/landingGetStartedButton.png";
 
 const LandingPage = () => {
+    const navigate = useNavigate(); // Make sure this is inside the component
   return (
     <div>
       <h1>Welcome to AdvocateAI</h1>
-      <button onClick={() => window.location.href = "/document-check"}>Get Started</button>
+      {/* <button onClick={() => window.location.href = "/document-check"}>Get Started</button> */}
+      <div className="landing-page-button">   
+         <img 
+        src={buttonImage} 
+        alt="Get Started" 
+        className="custom-button"
+        onClick={() => navigate("/document-check")} 
+      />
+        </div>
     </div>
   );
 };
